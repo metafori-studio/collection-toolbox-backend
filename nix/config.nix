d38b44e27b18daec1c4e61c44ca6b6cff29bd862
mtmn <44134862+mtmn@users.noreply.github.com>
2026-02-20 13:10:45 +0100

    chore: format nix (#12)
    * chore(nix): split into modules

---
+++ nix/config.nix
@@ -0,0 +1,11 @@
+{
+  dataDir = ".nix-data";
+  postgresDataDir = ".nix-data/postgres";
+  valkeyDataDir = ".nix-data/valkey";
+
+  postgresPort = "5432";
+  postgresUser = "postgres";
+  postgresDb = "collection_toolbox_backend";
+
+  valkeyPort = "6379";
+}
---
+++ nix/php.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+
+let
+  php = pkgs.php85.buildEnv {
+    extensions = (
+      { enabled, all }:
+      enabled
+      ++ (with all; [
+        pdo_pgsql
+        pgsql
+        redis
+      ])
+    );
+    extraConfig = ''
+      memory_limit = 256M
+      upload_max_filesize = 20M
+      post_max_size = 20M
+    '';
+  };
+in
+php
