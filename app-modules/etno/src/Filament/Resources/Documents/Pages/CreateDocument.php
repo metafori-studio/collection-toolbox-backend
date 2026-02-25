4274595c970f9781faefb1b2f29e2c06bf809adc
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2026-02-25 21:22:26 +0100

    feat(etno): add document resource

---
+++ app-modules/etno/src/Filament/Resources/Documents/Pages/CreateDocument.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Metafori\Etno\Filament\Resources\Documents\Pages;
+
+use Filament\Resources\Pages\CreateRecord;
+use Metafori\Etno\Filament\Resources\Documents\DocumentResource;
+
+class CreateDocument extends CreateRecord
+{
+    protected static string $resource = DocumentResource::class;
+}
---
+++ app-modules/etno/src/Filament/Resources/Documents/Pages/EditDocument.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Metafori\Etno\Filament\Resources\Documents\Pages;
+
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ForceDeleteAction;
+use Filament\Actions\RestoreAction;
+use Filament\Resources\Pages\EditRecord;
+use Metafori\Etno\Filament\Resources\Documents\DocumentResource;
+
+class EditDocument extends EditRecord
+{
+    protected static string $resource = DocumentResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make(),
+            ForceDeleteAction::make(),
+            RestoreAction::make(),
+        ];
+    }
+}
