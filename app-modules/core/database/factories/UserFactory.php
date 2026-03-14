4dbabc506dd4daa59a4c669cf55d8d635e20b798
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2026-03-14 21:17:08 +0100

    refactor(core): move User model

--- app-modules/core/database/factories/UserFactory.php
+++ app-modules/core/database/factories/UserFactory.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Database\Factories;
+namespace Metafori\Core\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Metafori\Core\Models\User>
  */
 class UserFactory extends Factory
 {
