0782198217955ec7f20854da584b7afcc806c4a3
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2026-03-15 23:35:16 +0100

    fix(core): add user properties docblock

--- app-modules/core/src/Models/User.php
+++ app-modules/core/src/Models/User.php
@@ -8,6 +8,16 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string|null $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
