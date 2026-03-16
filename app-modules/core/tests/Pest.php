33aa820dddb8a0c8304d8216cb7cff78a44b04cf
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2026-03-16 11:40:14 +0100

    test(core): add defaults for feature tests

--- app-modules/core/tests/Pest.php
+++ app-modules/core/tests/Pest.php
@@ -1,4 +1,9 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Metafori\Core\Tests\Concerns\InteractsWithStatefulHeaders;
+
 pest()->extend(Metafori\Core\Tests\TestCase::class)
+    ->use(RefreshDatabase::class)
+    ->use(InteractsWithStatefulHeaders::class)
     ->in('Feature');
