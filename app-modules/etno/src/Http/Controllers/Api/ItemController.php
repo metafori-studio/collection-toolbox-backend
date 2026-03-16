4678dc2242e873436c907575c9c7af28791e7e8e
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2026-03-16 16:49:42 +0100

    feat(etno): add item api controller

---
+++ app-modules/etno/src/Http/Controllers/Api/ItemController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Metafori\Etno\Http\Controllers\Api;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Metafori\Etno\Http\Resources\ItemResource;
+use Metafori\Etno\Repositories\ItemRepository;
+
+class ItemController
+{
+    public function __construct(
+        private readonly ItemRepository $repository,
+    ) {}
+
+    public function index(): ResourceCollection
+    {
+        $items = $this->repository->paginate();
+
+        return ItemResource::collection($items);
+    }
+
+    /**
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException<\Metafori\Etno\Models\Item>
+     */
+    public function show(string $id): ItemResource
+    {
+        $item = $this->repository->findOrFail($id);
+
+        return new ItemResource($item);
+    }
+}
