27fcc3633e636befd887682c15a15f51da7fc358
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2026-03-23 23:56:47 +0100

    feat(etno): add HasInheritable sections

--- app-modules/etno/src/Filament/Schemas/Components/AuthorsAndCreatorsSection.php
+++ app-modules/etno/src/Filament/Schemas/Components/AuthorsAndCreatorsSection.php
@@ -6,20 +6,26 @@ use Filament\Schemas\Components\Section;
 use Metafori\Etno\Filament\Forms\Components\AuthorsSelect;
 use Metafori\Etno\Filament\Forms\Components\OriginatorsRepeater;
 use Metafori\Etno\Filament\Forms\Components\ResearchersSelect;
+use Metafori\Etno\Filament\Schemas\Components\Concerns\HasInheritable;
 
 class AuthorsAndCreatorsSection extends Section
 {
+    use HasInheritable;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->heading('Authors and Creators')
-            ->schema([
+            ->schema(fn () => [
                 AuthorsSelect::make('authors')
+                    ->inheritable($this->inheritable)
                     ->columnSpanFull(),
                 ResearchersSelect::make('researchers')
+                    ->inheritable($this->inheritable)
                     ->columnSpanFull(),
                 OriginatorsRepeater::make('originators')
+                    ->inheritable($this->inheritable)
                     ->columnSpanFull(),
             ]);
     }
