b6cba82f8538beb05de0d2e58c0062bfd7cb0148
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2026-03-25 08:55:16 +0100

    feat(etno): update item on related entities updates

--- app-modules/etno/src/Models/Item.php
+++ app-modules/etno/src/Models/Item.php
@@ -22,6 +22,7 @@ use Metafori\Core\Models\Person;
 use Metafori\Core\Models\Region;
 use Metafori\Etno\Models\Concerns\HasDocumentMetadata;
 use Metafori\Etno\Models\Contracts\Inheritable;
+use Metafori\Etno\Models\Pivots\ItemPivot;
 
 class Item extends Model implements Inheritable
 {
@@ -110,12 +111,16 @@ class Item extends Model implements Inheritable
 
     public function authors(): BelongsToMany
     {
-        return $this->belongsToMany(Person::class, 'etno_item_authors')->orderByPivot('sort_order');
+        return $this->belongsToMany(Person::class, 'etno_item_authors')
+            ->using(ItemPivot::class)
+            ->orderByPivot('sort_order');
     }
 
     public function researchers(): BelongsToMany
     {
-        return $this->belongsToMany(Person::class, 'etno_item_researchers')->orderByPivot('sort_order');
+        return $this->belongsToMany(Person::class, 'etno_item_researchers')
+            ->using(ItemPivot::class)
+            ->orderByPivot('sort_order');
     }
 
     public function originators(): HasMany
@@ -125,12 +130,16 @@ class Item extends Model implements Inheritable
 
     public function keywords(): BelongsToMany
     {
-        return $this->belongsToMany(Keyword::class, 'etno_item_keyword')->orderByPivot('sort_order');
+        return $this->belongsToMany(Keyword::class, 'etno_item_keyword')
+            ->using(ItemPivot::class)
+            ->orderByPivot('sort_order');
     }
 
     public function researchCollections(): BelongsToMany
     {
-        return $this->belongsToMany(ResearchCollection::class, 'etno_item_research_collection')->orderByPivot('sort_order');
+        return $this->belongsToMany(ResearchCollection::class, 'etno_item_research_collection')
+            ->using(ItemPivot::class)
+            ->orderByPivot('sort_order');
     }
 
     public function document(): BelongsTo
