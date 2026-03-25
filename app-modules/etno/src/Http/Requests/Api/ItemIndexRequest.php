ec14cf32a256d161de5f447f4bae9a31a306a211
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2026-03-25 08:58:41 +0100

    docs(etno): properly document etno.items.index

--- app-modules/etno/src/Http/Requests/Api/ItemIndexRequest.php
+++ app-modules/etno/src/Http/Requests/Api/ItemIndexRequest.php
@@ -18,7 +18,7 @@ class ItemIndexRequest extends FormRequest
             /**
              * Specific model property mapped dynamically to a scalar. Supports comma separation for multiple sort properties. To sort descending, prepend a hyphen (`-`) to the target property name.
              *
-             * @example -time_period_start
+             * @example -type
              */
             'sort' => ['nullable', 'string'],
 
@@ -26,89 +26,75 @@ class ItemIndexRequest extends FormRequest
              * Map of filtering constraints.
              */
             'filter' => ['array'],
-
+            'filter.type' => ['array'],
             /**
              * Filter by Item types (matches any if multiple given).
              */
-            'filter.type' => ['array'],
             'filter.type.*' => ['string'],
-
+            'filter.language' => ['array'],
             /**
              * Filter by languages (matches any if multiple given).
              */
-            'filter.language' => ['array'],
             'filter.language.*' => ['string'],
-
+            'filter.accrual_method' => ['array'],
             /**
              * Filter by accrual methods (matches any if multiple given).
              */
-            'filter.accrual_method' => ['array'],
             'filter.accrual_method.*' => ['string'],
-
+            'filter.collection_method' => ['array'],
             /**
              * Filter by collection methods (matches any if multiple given).
              */
-            'filter.collection_method' => ['array'],
             'filter.collection_method.*' => ['string'],
-
+            'filter.access_rights' => ['array'],
             /**
              * Filter by access rights (matches any if multiple given).
              */
-            'filter.access_rights' => ['array'],
             'filter.access_rights.*' => ['string'],
-
+            'filter.license' => ['array'],
             /**
              * Filter by licenses (matches any if multiple given).
              */
-            'filter.license' => ['array'],
             'filter.license.*' => ['string'],
-
+            'filter.production_methods' => ['array'],
             /**
              * Filter by production methods (matches any if multiple given).
              */
-            'filter.production_methods' => ['array'],
             'filter.production_methods.*' => ['string'],
-
+            'filter.author.person_id' => ['array'],
             /**
              * Filter by the IDs of related authors (matches any if multiple given).
              */
-            'filter.author.person_id' => ['array'],
             'filter.author.person_id.*' => ['integer'],
-
+            'filter.researcher.person_id' => ['array'],
             /**
              * Filter by the IDs of related researchers.
              */
-            'filter.researcher.person_id' => ['array'],
             'filter.researcher.person_id.*' => ['integer'],
-
+            'filter.originator.person_id' => ['array'],
             /**
              * Filter by the IDs of related originators.
              */
-            'filter.originator.person_id' => ['array'],
             'filter.originator.person_id.*' => ['integer'],
-
+            'filter.keyword.id' => ['array'],
             /**
              * Filter by the IDs of mapped keywords.
              */
-            'filter.keyword.id' => ['array'],
             'filter.keyword.id.*' => ['integer'],
-
+            'filter.research_collection.id' => ['array'],
             /**
              * Filter by the IDs of related research collections.
              */
-            'filter.research_collection.id' => ['array'],
             'filter.research_collection.id.*' => ['integer'],
-
+            'filter.institution.id' => ['array'],
             /**
              * Filter by the ID of the related institution.
              */
-            'filter.institution.id' => ['array'],
             'filter.institution.id.*' => ['integer'],
-
+            'filter.project.id' => ['array'],
             /**
              * Filter by the ID of the related project.
              */
-            'filter.project.id' => ['array'],
             'filter.project.id.*' => ['integer'],
             'filter.country.id' => ['array'],
             /**
