29a70c0a63eef0cf079e7e306b349e4b9f6b425d
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2026-03-30 00:48:47 +0200

    refactor(etno): unique suffix validation in context of document

--- app-modules/etno/src/Filament/Forms/Components/SuffixInput.php
+++ app-modules/etno/src/Filament/Forms/Components/SuffixInput.php
@@ -3,7 +3,9 @@
 namespace Metafori\Etno\Filament\Forms\Components;
 
 use Filament\Forms\Components\TextInput;
+use Illuminate\Validation\Rules\Unique;
 use Livewire\Component;
+use Tpetry\PostgresqlEnhanced\Query\Builder;
 
 class SuffixInput extends TextInput
 {
@@ -14,6 +16,11 @@ class SuffixInput extends TextInput
         $this->label('Composite ID')
             ->prefix(fn (Component $livewire) => $livewire->parentRecord?->id)
             ->required()
+            ->unique(
+                ignoreRecord: true,
+                modifyRuleUsing: fn (Unique $rule, Component $livewire) => $rule
+                    ->where(fn (Builder $query) => $query->where('document_id', $livewire->parentRecord?->id))
+            )
             ->maxLength(255);
     }
 }
