c87b6cd4a88034dd776d4711d84bd9bd6d084d10
mtmn <miro@haravara.org>
2026-03-30 12:48:54 +0200

    feat: add dockerfile

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{path?}', function (?string $path = null) {
+    if ($path && file_exists(public_path($path))) {
+        return response()->file(public_path($path));
+    }
+
+    return response()->file(public_path('index.html'));
+})->where('path', '.*');
