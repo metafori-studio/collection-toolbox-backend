bd3898739e8f494136cfe90b2b531dd04289b863
mtmn <miro@haravara.org>
2026-04-21 12:42:59 +0200

    fix(ci): disable otel by default

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Keepsuit\LaravelOpenTelemetry\LaravelOpenTelemetryServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,15 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
             $this->app->register(TelescopeServiceProvider::class);
+        }
+
+        // Prevent OpenTelemetry from loading if all exporters are set to null
+        $tracesExporter = config('opentelemetry.traces.exporter');
+        $metricsExporter = config('opentelemetry.metrics.exporter');
+        $logsExporter = config('opentelemetry.logs.exporter');
+
+        if ($tracesExporter === 'null' && $metricsExporter === 'null' && $logsExporter === 'null') {
+            $this->app->register(LaravelOpenTelemetryServiceProvider::class, false);
         }
     }
 
