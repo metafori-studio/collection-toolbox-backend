7db9dde806aefcac2534903a58f7a9adccab1efb
rastislav-chynoransky <rastislav.chynoransky@gmail.com>
2026-05-07 18:29:11 +0200

    chore: wrap permission cache invalidation in afterCommit hook in migration

--- app-modules/core/database/migrations/2026_03_15_000001_create_permission_tables.php
+++ app-modules/core/database/migrations/2026_03_15_000001_create_permission_tables.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -114,9 +115,11 @@ return new class extends Migration
             $table->primary([$pivotPermission, $pivotRole], 'role_has_permissions_permission_id_role_id_primary');
         });
 
-        app('cache')
-            ->store(config('permission.cache.store') != 'default' ? config('permission.cache.store') : null)
-            ->forget(config('permission.cache.key'));
+        DB::afterCommit(function () {
+            app('cache')
+                ->store(config('permission.cache.store') != 'default' ? config('permission.cache.store') : null)
+                ->forget(config('permission.cache.key'));
+        });
     }
 
     /**
