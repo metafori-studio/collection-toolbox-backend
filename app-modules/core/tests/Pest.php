fad0a57d8748bba178c99e232c77a7f5e3a76cc8
rastislav-chynoransky <rastislav.chynoransky@gmail.com>
2026-05-26 22:10:23 +0200

    test: run module tests from apps

--- app-modules/core/tests/Pest.php
+++ app-modules/core/tests/Pest.php
@@ -4,12 +4,14 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Metafori\Core\Database\Seeders\RoleSeeder;
 use Metafori\Core\Tests\Concerns\InteractsWithStatefulHeaders;
 use Metafori\Core\Tests\TestCase;
+use Pest\Livewire\InteractsWithLivewire;
 
 pest()->extend(TestCase::class)
     ->in(__DIR__.'/Unit');
 
 pest()->extend(TestCase::class)
     ->use(RefreshDatabase::class)
+    ->use(InteractsWithLivewire::class)
     ->use(InteractsWithStatefulHeaders::class)
     ->in(__DIR__.'/Feature');
 
