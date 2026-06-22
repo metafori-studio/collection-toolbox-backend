24bcff5da953db697dc21a94b7eab639822d35c1
mtmn <miro@haravara.org>
2026-06-22 22:00:00 +0200

    feat(archeo): trigger watermarking after compression, add tests

--- app-modules/archeo/src/Listeners/CompressPdfOnUploadListener.php
+++ app-modules/archeo/src/Listeners/CompressPdfOnUploadListener.php
@@ -3,7 +3,9 @@
 namespace Metafori\Archeo\Listeners;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Bus;
 use Metafori\Archeo\Jobs\CompressPdfJob;
+use Metafori\Archeo\Jobs\WatermarkPdfJob;
 use Metafori\Archeo\Models\Activity;
 use Metafori\Core\Models\User;
 use Spatie\MediaLibrary\MediaCollections\Events\MediaHasBeenAddedEvent;
@@ -25,6 +27,15 @@ class CompressPdfOnUploadListener
         /** @var User|null $user */
         $user = Auth::user();
 
-        CompressPdfJob::dispatch($media->id, $user);
+        $jobs = [new CompressPdfJob($media->id, $user)];
+
+        // Watermark the compressed PDF once compression finishes.
+        $watermarkImage = config('archeo.watermark_image');
+
+        if ($watermarkImage && file_exists($watermarkImage)) {
+            $jobs[] = new WatermarkPdfJob($media->id, $user);
+        }
+
+        Bus::chain($jobs)->dispatch();
     }
 }
